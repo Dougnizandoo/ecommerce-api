81b24c38f9e8d60c05d894cf919f59ed8203c2d5
Doug Aquino <rpa.dougaquino@gmail.com>
2024-10-17 11:03:46 -0300

    Teste de Modificação

--- hello.py
+++ hello.py
@@ -9,5 +9,10 @@ def hello_world():
     return "<p>hello World! good luck!</p>"
 
 
+@app.route("/teste")
+def teste_git():
+    return "<body><p>teste do git!</p></body>"
+
+
 if __name__ == '__main__':
     app.run(host="localhost", port=1337, debug=True)
