40df3daa155288300c43b5579ef5b31995050de5
Dougnizandoo <rpa.dougaquino@gmail.com>
2024-10-17 10:46:59 -0300

    HelloWorld

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "<p>hello World! good luck!</p>"
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=1337, debug=True)
